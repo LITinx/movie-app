37cf4940e69e0c25e3756756fff1ca0b53ce8c73
Daniel <kuruchbekov2006dan@gmail.com>
2022-05-04 00:36:31 +0600

    adding overview

--- src/js/cards.js
+++ src/js/cards.js
@@ -57,7 +57,7 @@ dropDownMenu.addEventListener('click', () => {
 	ulMenu.classList.toggle('hidden');
 });
 btnNext.addEventListener('click', () => {
-	if (pages < 15) {
+	if (pages < 40) {
 		pages++;
 		wrapper.innerHTML = '';
 		fetchingMovies(search);
@@ -116,17 +116,21 @@ const btnDisable = () => {
 };
 // https://api.themoviedb.org/3/movie/550?api_key=
 const fetchPopularMovies = async (genre) => {
-	const res = await fetch(
-		`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=${pages}`,
-	);
-	const data = await res.json();
-	console.log(data);
-	if (data.success !== false) {
-		for (let i = 0; i < 20; i++) {
-			cards(data.results[i]);
+	try {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/movie/${genre}?api_key=${API_KEY}&language=en-US&page=${pages}`,
+		);
+		const data = await res.json();
+		console.log(data);
+		if (data.success !== false) {
+			for (let i = 0; i < 20; i++) {
+				cards(data.results[i]);
+			}
 		}
+		pagesChecker();
+	} catch (err) {
+		console.error(`Error ${err}`);
 	}
-	pagesChecker();
 };
 const fetchingMovies = (searchValue) => {
 	if (movieStatus === 'popular') {
@@ -166,13 +170,13 @@ const pagesChecker = () => {
 
 const cards = (movie) => {
 	wrapper.innerHTML += `
-	<div class="p-4 w-48 card">
+		<div class="p-4 w-48 card relative">
       <img loading="lazy" src="${
 				movie.poster_path
 					? `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`
 					: 'https://www.ceeol.com/images/no-image.jpg'
 			}" alt="${movie.title ? movie.title : 'film photo'}"
-        class="w-full rounded-lg shadow-sm shadow-black cursor-pointer object-cover">
+        class="w-full rounded-lg shadow-sm shadow-black cursor-pointer object-cover cardImg">
       <div class="relative">
         <span
           class="text-sm border-[3px] rounded-full p-1 ${
@@ -188,6 +192,33 @@ const cards = (movie) => {
         </h4>
         <p class="text-gray-500 text-sm">
           ${movie.release_date ? movie.release_date : 'Coming Out'}</p>
-      </div>
+      </div> 
+				<div class="min-w-fit min-h-fit bg-slate-300 absolute top-10 flex-col sm:flex-row cardModal rounded-2xl z-10"
+					id="modalMenu">
+				<img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${
+					movie.backdrop_path
+				}" alt=""
+						class="w-60 object-cover h-80 p-3 ">
+				<div class="p-4 md:pl-0 pl-4 pt-2 md:p-6 bg-slate-300 rounded-2xl">
+					<h2 class="font-bold  text-2xl pb-5">${
+						movie.title
+					} <span class="font-normal">(${movie.release_date
+		.split('-')
+		.shift()})</span></h2>
+					<div class="flex gap-4 items-center">
+						<div class="">
+							<span
+								class="text-lg border-[3px] rounded-full p-2  border-green-400 rate font-bold text-white bg-gray-800">78<span
+									class="text-[8px]">%</span></span>
+						</div>
+						<h6 class="text-md">User Score</h6>
+					</div>
+					<div class="pt-4">
+						<h4 class="text-lg font-medium">Overview</h4>
+						<p class="para">${movie.overview}</p>
+					</div>
+				</div>
+			</div>
+		</div>
   `;
 };
